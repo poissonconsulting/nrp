4629d1d6be6ff6e0f9b5aea50bdb5b74b86a4036
Evan <evanamiesgalonski@gmail.com>
2019-02-07 09:22:25 -0800

    always bind with nrp_read_ctd, basic upload function

---
+++ R/upload.R
@@ -0,0 +1,22 @@
+#' Upload data to nrp database
+#'
+#' @param data the object name of the data to be uploaded
+#' @param conn A formal SQlite connection
+#' @inheritParams readwritesqlite::rws_write_sqlite
+#'
+#' @export
+#'
+
+
+nrp_upload <- function(data, conn){
+
+  ctd_names <- c('DateTime', 'Depth', 'Temperature', 'Oxygen', 'Oxygen2', 'Conductivity', 'Conductivity2',
+                 'Salinity', 'Backscatter', 'Fluorescence', 'Frequency', 'Flag', 'Pressure')
+
+  if(identical(names(data), ctd_names)){
+    check_ctd_data(data, exclusive = TRUE, order = TRUE)
+    rws_write_sqlite(ctds, conn = conn, x_name = "CTD")
+  } else {
+    err("Columns names do not match any existing data in database.")
+  }
+}
--- tests/testthat/test-ctd.R
+++ tests/testthat/test-ctd.R
@@ -32,23 +32,4 @@ test_that("nrp_read_ctds works", {
   data <- nrp_read_ctd(path)
   expect_identical(data,  list(x = 1)[-1])
 
-  path <-  system.file("extdata", "ctd/2018", package = "nrp", mustWork = TRUE)
-  data <- nrp_read_ctd(path, bind = FALSE)
-  expect_is(data, "list")
-  expect_identical(length(data), 1L)
-  expect_match(names(data), path)
-
-  expect_is(data[[1]], "tbl_df")
-  expect_match(attr(data[[1]], "path"), path)
-  expect_is(attr(data[[1]], "flob"), "flob")
-
-
-  data <- nrp_read_ctd(path, recursive = TRUE, bind = FALSE)
-  expect_is(data, "list")
-  expect_identical(length(data), 3L)
-  expect_match(names(data), path)
-
-  expect_is(data[[2]], "tbl_df")
-  expect_match(attr(data[[2]], "path"), path)
-  expect_is(attr(data[[2]], "flob"), "flob")
 })
