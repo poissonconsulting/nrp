1c38e54fcc0841dbd5286d3076cfaa906e02709f
Evan <evanamiesgalonski@gmail.com>
2019-02-08 12:14:06 -0800

    modify nrp_upload function

--- R/upload.R
+++ R/upload.R
@@ -1,13 +1,18 @@
 #' Upload data to nrp database
 #'
 #' @param data the object name of the data to be uploaded
-#' @param conn A formal SQlite connection
+#' @param db_path The oath to the database file
 #' @inheritParams readwritesqlite::rws_write_sqlite
 #'
 #' @export
 #'
-nrp_upload <- function(data, conn, commit = TRUE){
+nrp_upload <- function(data, db_path, commit = TRUE, strict = TRUE, silent = TRUE){
 
- rws_write_sqlite(data, commit = commit, conn = conn, x_name = assess_data_type(data))
+  conn <- readwritesqlite::rws_open_connection(db_path)
+  on.exit(readwritesqlite::rws_close_connection(conn))
 
+  readwritesqlite::rws_write_sqlite(data, commit = commit, strict = strict, silent = silent,
+                                   x_name = assess_data_type(data), conn = conn)
 }
+
+
