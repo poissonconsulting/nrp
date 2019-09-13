feab3caaa182151fd9dcf4042d919046e07fce77
joethorley <joe@poissonconsulting.ca>
2019-09-13 10:31:08 -0700

    fix tests

--- tests/testthat/test-create-db.R
+++ tests/testthat/test-create-db.R
@@ -5,5 +5,5 @@ test_that("nrp-create-db works", {
   conn <- nrp_create_db(path  = ":memory:", ask = FALSE)
   teardown(DBI::dbDisconnect(conn))
 
-  expect_identical(readwritesqlite::check_sqlite_connection(conn, connected = TRUE), conn)
+  expect_true(readwritesqlite::chk_sqlite_conn(conn, connected = TRUE))
 })
