f5ef259be097ed980576dd99b90d2757878418de
Evan <evanamiesgalonski@gmail.com>
2021-04-15 14:29:02 -0700

    functions to read in mysid and zoo raw data

---
+++ R/zoo.R
@@ -0,0 +1,46 @@
+#' Read zooplankton raw data file
+#'
+#' @param path A string of the path to the file.
+#' @param db_path The SQLite connection object or path to the nrp SQLite database.
+#' @param system The system 'arrow' or 'kootenay'. If null, the system is detected from the file name.
+#' @return A tibble
+#' @export
+nrp_read_zooplankton_file <- function(path, db_path = getOption("nrp.db_path",
+                                                                file.choose()), system = NULL) {
+  check_file_exists(path)
+
+  if(is.null(system)){
+    if(str_detect(tolower(basename(path)), "^ar")){
+      system <- "AR"
+    } else if(str_detect(tolower(basename(path)), "^kl")) {
+      system <- "KL"
+    } else {
+      err("System cannot be detected from file name. Please set system argument to 'arrow' or 'kootenay'.")
+    }
+  } else {
+    system <- tolower(system)
+    if(!system %in% c("arrow", "kootenay")) err("'system' must be one of 'arrow', 'kootenay'.")
+    system <- ifelse(system == "arrow", "AR", "KL")
+  }
+
+  if(!inherits(db_path, "SQLiteConnection")){
+    db_path <- connect_if_valid_path(path = db_path)
+    on.exit(readwritesqlite::rws_disconnect(conn = db_path))
+  }
+
+  zoo_col_types <- c("text", "numeric", "numeric", "text",  "date",  "date",  "numeric",  "text",  "text",
+                     "numeric", "text", "text", rep("numeric", 154), "text", "text", "text")
+
+  data <- readxl::read_excel(path, col_types = zoo_col_types)
+  check_zoo_raw_data(data, exclusive = TRUE, order = TRUE)
+
+  data %<>% filter(!all_na(data)) %>%
+    mutate(Station = paste0(system, .data$Station))
+
+  sites <- nrp_download_sites(db_path = db_path)
+
+  if(!all(unique(data$Station) %in% sites$SiteID)) err("Unknown Stations in raw data.")
+  chk::check_key(data, key = c("Date1", "Station", "Replicate", "FileName"))
+
+  data
+}
