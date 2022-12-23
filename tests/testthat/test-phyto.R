8158b2a37968be7e726051e077c141fedfd2670f
Evan <evanamiesgalonski@gmail.com>
2022-12-22 16:09:46 -0800

    test read in of raw phyto data

--- tests/testthat/test-phyto.R
+++ tests/testthat/test-phyto.R
@@ -20,3 +20,35 @@ test_that("nrp_add_phyto_species works", {
   expect_identical(spp, spp_db)
 
 })
+
+test_that("nrp_read_plytoplankton_file works", {
+
+  conn <- nrp_create_db(path = ":memory:", ask = FALSE)
+  teardown(DBI::dbDisconnect(conn))
+
+  path <- system.file("extdata", "phyto/phyto1.xlsx",
+                      package = "nrp", mustWork = TRUE)
+
+  wrong_path <- system.file("extdata", "ar-empty.rtf", package = "nrp", mustWork = TRUE)
+
+  data <- nrp_read_phyto_file(path = path, db_path = conn)
+
+  expect_is(data, "tbl_df")
+  expect_identical(nrow(data), 15L)
+
+  expect_error(nrp_read_phyto_file(path = wrong_path, db_path = conn),
+               "Please ensure input data is a valid excel spreadsheet \\(.xlsx\\).")
+
+  path2 <- system.file("extdata", "phyto/more/phyto2.xlsx",
+                      package = "nrp", mustWork = TRUE)
+
+  expect_warning(nrp_read_phyto_file(path = path2, db_path = conn),
+               "Sites in input data not present in 'Sites' table in database: 'AR12'.")
+
+  path3 <- system.file("extdata", "phyto/more/phyto3.xlsx",
+                       package = "nrp", mustWork = TRUE)
+
+  expect_warning(nrp_read_phyto_file(path = path3, db_path = conn),
+                 "Taxa in input data not present in 'PhytoplanktonSpecies' table in database: 'New Species'.")
+
+})
